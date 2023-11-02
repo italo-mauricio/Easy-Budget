3f14d5914438d9ebafcaf7a6c6c630c417026343
italo <italomauricio98@gmail.com>
2023-11-02 18:18:03 -0300

    update

--- easy-budget-project/src/App.js
+++ easy-budget-project/src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Switch, Routes, Route, Container, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Company from './pages/Company';
+import Contact from './pages/Contact';
+import NewProject from './pages/NewProject';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Budget</p>
-    </div>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/company' element={<Company />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/newproject' element={<NewProject />} />
+      </Routes>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
