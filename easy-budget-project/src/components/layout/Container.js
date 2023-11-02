72cd21366e469307e7e2a7b5b95ca04907b20ee4
italo <italomauricio98@gmail.com>
2023-11-02 18:42:03 -0300

    update

--- easy-budget-project/src/components/layout/Container.js
+++ easy-budget-project/src/components/layout/Container.js
@@ -1,9 +1,9 @@
-import styles from './Container.modules.css';
+import styles from './Container.module.css';
 
 
 
 function Container(props) {
-    return <div>{props.children}</div>;
+    return <div className={`${styles.container} ${styles[props.customClass]}`}>{props.children}</div>;
 }
 
 export default Container
